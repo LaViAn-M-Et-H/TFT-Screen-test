d38a2de327e5e29d2c8d896bad000cfe7b959960
LaViAn-M-Et-H <susanomemia@gmail.com>
2025-07-22 20:54:50 +0700

    Add pull-up and pull-down mode

--- tft_test/main/main.c
+++ tft_test/main/main.c
@@ -245,6 +245,9 @@ void init_display() {
     gpio_config_t io_conf = {
         .pin_bit_mask = (1ULL << PIN_NUM_DC) | (1ULL << PIN_NUM_RST) | (1ULL << PIN_NUM_BL),
         .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
     };
     gpio_config(&io_conf);
 
